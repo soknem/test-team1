77424fd2deb9ba67cfb2e858c3d3c47764ffcffe
povsoknem <povsoknem@gmail.com>
2024-09-13 23:14:58 +0700

    Auto commit

--- test-team1/Program.cs
+++ test-team1/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows.Forms;
-using test_team1.Ui;
 
 namespace test_team1
 {
@@ -14,7 +13,7 @@ namespace test_team1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Ui1());
+            Application.Run(new Form1());
         }
     }
 }
