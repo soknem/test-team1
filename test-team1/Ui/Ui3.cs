c997588dc0dc6bf40bb4be5741c7d43a2419bf41
povsoknem <povsoknem@gmail.com>
2024-09-13 23:40:58 +0700

    Auto commit

---
+++ test-team1/Ui/Ui3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_team1.Ui
+{
+    public partial class Ui3 : Form
+    {
+        public Ui3()
+        {
+            InitializeComponent();
+        }
+    }
+}
