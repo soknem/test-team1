631eee6593047a30248b3dd8035be27dc01d97c4
povsoknem <povsoknem@gmail.com>
2024-09-13 23:07:22 +0700

    change run form

--- test-team1/Program.cs
+++ test-team1/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using test_team1.Ui;
 
 namespace test_team1
 {
@@ -16,7 +14,7 @@ namespace test_team1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Ui1());
         }
     }
 }
