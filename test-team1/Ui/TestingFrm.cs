f30e6edcf8b40bf92ba5bf1265a166c6f500763b
phatphea <phartphea853@gmail.com>
2024-09-14 13:47:00 +0700

    testing2

---
+++ test-team1/Ui/TestingFrm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_team1.Ui
+{
+    public partial class TestingFrm : Form
+    {
+        public TestingFrm()
+        {
+            InitializeComponent();
+        }
+    }
+}
